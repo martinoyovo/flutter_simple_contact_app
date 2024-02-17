94f54b01bdda32eeff12624c39bd3cffd948fdf2
Martino Yovo <martinosoyen@gmail.com>
2024-02-17 17:12:19 -0500

    App Colors

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Screen background
+  static const Color screenGrayBackgroundDark = Color.fromRGBO(244, 247, 247, 1.0);
+
+  static const BoxDecoration grayBackgroundBoxDecoration = BoxDecoration(color: screenGrayBackgroundDark);
+
+  // Backgrounds and strokes
+  static const Color neutral0 = Color.fromRGBO(255, 255, 255, 1.0);
+  static const Color neutral50 = Color.fromRGBO(249, 249, 249, 1.0);
+  static const Color neutral100 = Color.fromRGBO(232, 236, 236, 1.0);
+  static const Color neutral200 = Color.fromRGBO(211, 216, 218, 1.0);
+
+  //Default Text color
+  static const Color neutral300 = Color.fromRGBO(128, 135, 136, 1.0);
+  static const Color neutral400 = Color.fromRGBO(92, 97, 98, 1.0);
+  static const Color neutral500 = Color.fromRGBO(7, 29, 30, 1.0);
+
+  static const Color greenBackground = Color.fromRGBO(127, 191, 90, 0.1);
+  static const Color circularChartExtraNeutral = Color.fromRGBO(190, 197, 199, 1.0);
+
+  // Green
+  static const Color primary = Colors.blue;
+  static const Color redColor = Colors.red;
+}
