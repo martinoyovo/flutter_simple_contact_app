61c472d14a51020a1486d43ea3193cad5684f7f1
Martino Yovo <martinosoyen@gmail.com>
2024-02-17 17:13:02 -0500

    Servie locator

---
+++ lib/locator.dart
@@ -0,0 +1,8 @@
+import 'package:get_it/get_it.dart';
+import 'package:simple_contact_app/services/contact.service.dart';
+
+GetIt get locator => GetIt.instance;
+
+setupLocator() {
+  locator.registerLazySingleton(() => ContactService());
+}
