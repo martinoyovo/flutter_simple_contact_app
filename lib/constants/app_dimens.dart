4a33551f15592bf0a04ea6c479927b2c07f8df9c
Martino Yovo <martinosoyen@gmail.com>
2024-02-17 17:12:34 -0500

    Sizes and dimensions

---
+++ lib/constants/app_dimens.dart
@@ -0,0 +1,37 @@
+class AppDimens {
+  static const double _defaultGridSize = 8;
+
+  static const double margin0_5 = _defaultGridSize * 0.5;
+  static const double margin1 = _defaultGridSize * 1;
+  static const double margin1_5 = _defaultGridSize * 1.5;
+  static const double margin2 = _defaultGridSize * 2;
+  static const double margin2_5 = _defaultGridSize * 2.5;
+  static const double margin3 = _defaultGridSize * 3;
+  static const double margin4 = _defaultGridSize * 4;
+  static const double margin4_5 = _defaultGridSize * 4.5;
+  static const double margin5 = _defaultGridSize * 5;
+  static const double margin6_5 = _defaultGridSize * 6.5;
+  static const double margin10 = _defaultGridSize * 10;
+
+  static const double radius1 = 10;
+  static const double radius2 = 12;
+  static const double radius3 = 24;
+
+  static const double fontSize0 = 10;
+  static const double fontSize1 = 12;
+  static const double fontSize2 = 14;
+  static const double fontSize3 = 16;
+  static const double fontSize4 = 20;
+  static const double fontSize5 = 24;
+  static const double fontSize6 = 28;
+  static const double fontSize7 = 32;
+  static const double fontSize8 = 36;
+  static const double fontSize9 = 40;
+  static const double fontSize10 = 48;
+
+
+  static const double iconSize = 48;
+  static const double miniIconSize = 30;
+  static const double avatarSize = 70;
+  static const double bigAvatarSize = 120;
+}
